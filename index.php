d2a912fd97628d1902a576a54c9f7675a78e9bf6
sabri <sabrinatwitter6@gmail.com>
2023-11-23 16:35:47 +0100

    add function

--- index.php
+++ index.php
@@ -15,6 +15,29 @@
     <input type="submit" value="Genera Password">
 </form>
 
+<?php 
+function isPasswordLengthSpecified() {
+    return isset($_GET['length']) && !empty($_GET['length']);
+}
+
+// questa condizione verifica se la richiesta HTTP è di tipo GET
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    // verifica se è stata specificata una lunghezza per la password nella richiesta GET utilizzando la funzione 
+    if (isPasswordLengthSpecified()) {
+         // ottiene e valida la lunghezza della password
+        $passwordLength = filter_input(INPUT_GET, 'length', FILTER_VALIDATE_INT);
+        // controlla se la lunghezza è un numero intero valido compreso tra 10 e 15
+        if ($passwordLength !== false && $passwordLength >= 10 && $passwordLength <= 15) {
+        // se la lunghezza è valida allora esce il messaggio positivo
+            echo "La lunghezza della password specificata è valida: $passwordLength";
+        }  else {
+        // altrimenti negativo
+            echo "Si prega di specificare la lunghezza della password.";
+        }
+    }
+}
+
+?>
 
 </body>
 </html>
